a41c80e80324d4505640779bd6312f581878b0a3
liujiazhu <liujiazhu@meorient.com>
2021-09-30 17:52:07 +0800

    修改文件名

---
+++ 多页面入口打包css和js/js/common.js
@@ -0,0 +1,5 @@
+function clg1(){
+    console.log('我是一段公共的js代码');
+}
+
+clg1();
--- 多页面入口打包css和js/js/entry2.js
+++ 多页面入口打包css和js/js/entry2.js
@@ -1,6 +1,8 @@
 import '../css/common.scss'
 import '../css/module2.css'
+import {clg1} from './module1/test1'
 function clickBtn1(){
     const btn21 = document.getElementById('btn2');
     console.log(btn21,'222')
 }
+clg1();
